410bc84ef9b631e8803f1ab6d413f7a95cb80d25
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2019-02-03 23:18:24 -0200

    Refactor remove logic restore activity driver from app.vue

--- src/App.vue
+++ src/App.vue
@@ -22,44 +22,13 @@ export default {
       //
     };
   },
-  methods: {
-    loadUserData(driver) {
-      if (!driver) {
-        if (this.$route.fullPath !== '/')
-          this.$router.push({ name: 'Home' })
-      } else {
-        switch (driver.status) {
-          case "PENDING_DOCS":
-            this.$router.push({ name: "FinishForm" });
-            break;
-
-          case "PENDING_BOOKING":
-            this.$router.push({
-              name: "DoPayment",
-              query: { booking: driver.booking.id }
-            });
-            break;
-
-          default:
-            if (sessionStorage.getItem('kovi_code2fs'))
-              return this.$router.push({
-                name: "FirstData"
-              })
-
-            return this.$router.push({
-              name: "Home"
-            })
-        }
-      }
-    }
-  },
   mounted() {
     const driver = this.$store.getters["lead/driver"];
-    const sessionPhoneNumber = sessionStorage.getItem('kovi_phone')
+    const sessionPhoneNumber = sessionStorage.getItem("kovi_phone");
     if (sessionPhoneNumber && driver.phone_number) {
       this.$store.dispatch("lead/createDriver", driver.phone_number);
     }
-    this.loadUserData(driver);
-  },
+    this.$store.dispatch("lead/restoreActivity");
+  }
 };
 </script>
