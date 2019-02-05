54f52981c32407e658e50a5b7ac9446723093f58
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2019-02-05 12:24:33 -0200

    Refactor remove mode 'no-cors'

--- src/services/ApolloClient.js
+++ src/services/ApolloClient.js
@@ -4,9 +4,6 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const httpLink = new HttpLink({
   uri: process.env.VUE_APP_KOVI_GRAPHQL_URL,
-  fetchOptions: {
-    mode: 'no-cors'
-  }
 });
 const cache = new InMemoryCache();
 const apolloClient = new ApolloClient({
